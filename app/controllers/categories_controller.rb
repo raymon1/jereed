c55c8c3ddac864fa5952b1db0f8166bde3b2ef78
ramy farid <ramyfarid922@gmail.com>
2015-09-27 14:30:12 +0200

    Finished a minimal controller logic

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,7 +6,6 @@ class CategoriesController < ApplicationController
 
 	def show
 		@category = Category.find(params[:id])
-
 	end
 
 
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,18 @@
 class PagesController < ApplicationController
 	# The pages controller will render pages
+	def home
+
+	end
+
+
+	def about_us
+
+	end
+
+	def contact_us
+
+	end
+
+	
+
 end
