899a7b2fb47f4b0c7777b07135598428a5b9f650
raymon1 <raymonmina@gmail.com>
2015-09-27 13:12:01 +0200

    admin panel created and migration (without images)

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,9 @@
 class Category < ActiveRecord::Base
 	has_many :products
+
+	has_attached_file :image, styles: {large: "600X600>", medium: "300x300>", thumb: "100x100>" }
+	attr_accessor :image_file_name, :image_content_type, :image_file_size, :image_updated_at
+
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+
 end
