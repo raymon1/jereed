6fa8c03da92576e1f0d48aa19ceb6705b433fc34
raymon1 <raymonmina@gmail.com>
2015-09-28 13:05:29 +0200

    heroku for better

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150927112317) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
