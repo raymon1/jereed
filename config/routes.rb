528cf02390592ecdb9b271dd1e44c11c1461178d
raymon1 <raymonmina@gmail.com>
2015-10-02 22:33:47 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   root 'pages#home'
+  get 'categories' => 'categories#index'
+  get 'categories/:id' => 'categories#show'
+  get 'products' => 'products#index'
+  get 'products/:id' => 'products#show'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
