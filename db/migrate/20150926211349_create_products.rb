ff9750d6021185db05b24c8d85a65a8081631ee7
raymon1 <raymonmina@gmail.com>
2015-10-04 13:24:16 +0200

    added product 3d model link field

--- db/migrate/20150926211349_create_products.rb
+++ db/migrate/20150926211349_create_products.rb
@@ -2,6 +2,7 @@ class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
     	t.string :name
+    	t.string :model_link
     	t.text :description
     	t.references :category
 
